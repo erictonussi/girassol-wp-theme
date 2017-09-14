97d4ed24dd594eb1aadab372014083054cea36c1
Eric Tonussi <erictonussi@gmail.com>
2017-09-14 17:11:06 -0300

    header + inicio da index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,44 @@
 @extends('layouts.app')
 
+@section('top-content')
+  <section class="metaslider">
+    {!! do_shortcode("[metaslider id=36]"); !!}
+  </section>
+
+  <section class="cardapio">
+    <div class="container">
+      <h2 class="raminho text-center">Cardápio</h2>
+      <div class="d-flex justify-content-center justify-content-md-between flex-wrap">
+        <?php
+        $menu_types = get_terms(array(
+          'taxonomy' => 'rm-menu-type',
+          'hide_empty' => false,
+          // 'meta_key' => 'tax_position',
+          // 'orderby' => 'tax_position'
+        ));
+
+        foreach ( $menu_types as $menu_type ) {?>
+          <!-- <div class=""> -->
+            <a class="menu-type" href="#">
+              <?php the_term_thumbnail( $menu_type->term_id, $size = 'medium', 'class=thumbnail' ) ?>
+              <!-- <h4 class="title"><?php echo $menu_type->name; ?></h4> -->
+              <h4 class="title"><?php echo $menu_type->description; ?></h4>
+            </a>
+            <!-- <div class="menu-type">
+              <h4 class="title"><?php echo $menu_type->name; ?></h4>
+            </div>
+            <div class="menu-type">
+              <h4 class="title"><?php echo $menu_type->name; ?></h4>
+            </div> -->
+          <!-- </div> -->
+        <?php } ?>
+      </div>
+    </section>
+  </div>
+@endsection
+
 @section('content')
+
   @include('partials.page-header')
 
   @if (!have_posts())
