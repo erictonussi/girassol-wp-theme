6ab1fc4ee5646286fe78f68cd318cfbeacedd8f7
Eric Tonussi <erictonussi@gmail.com>
2017-09-16 11:53:47 -0300

    implementando scroll suave

--- resources/assets/scripts/routes/home.js
+++ resources/assets/scripts/routes/home.js
@@ -1,6 +1,31 @@
 export default {
-  init() {
-    // JavaScript to be fired on the home page
+  init() { //console.log(123);
+    // Select all links with hashes
+    $('a[href*="#"]')
+      // Remove links that don't actually link to anything
+      .not('[href="#"]')
+      .not('[href="#0"]')
+      .click(function(event) { //console.log(event);
+        // On-page links
+        if (
+          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+          && location.hostname == this.hostname
+        ) {
+          // Figure out element to scroll to
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          // Does a scroll target exist?
+          if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+
+            $('html, body').animate({
+              scrollTop: target.offset().top,
+            }, 400, function() {
+            });
+          }
+        }
+      });
   },
   finalize() {
     // JavaScript to be fired on the home page, after the init JS
